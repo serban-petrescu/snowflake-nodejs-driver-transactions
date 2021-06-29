38ab8c1e337f8368ac35fb7e1225be6a43ed4366
Serban Petrescu <Serban.Petrescu@outlook.com>
2021-06-29 14:44:14 +0300

    Adds small PoC for exporting data to S3.

--- README.md
+++ README.md
@@ -6,9 +6,16 @@ A very small PoC that shows how transactions can be used with Snowflake's NodeJS
  - Clone the repository and do a `npm ci` in the root folder.
  - Create a `.env` file in the root folder with the following contents (adjust the values accordingly):
 
-```
-SNOWFLAKE_ACCOUNT_NAME=abc1234.us-east-1
+```properties
+SNOWFLAKE_ACCOUNT=abc1234.us-east-1
 SNOWFLAKE_USERNAME=USERNAME
 SNOWFLAKE_PASSWORD=PASSWORD
+
+# Optional - only if you want to try out the export as well:
+S3_LOCATION=s3://bucket/prefix/
+S3_AWS_ACCESS_KEY_ID=KEY
+S3_AWS_SECRET_ACCESS_KEY=SECRET
+S3_AWS_SESSION_TOKEN=TOKEN (obtained via aws sts get-session-token)
+
 ```
  - Run `npm start`.
--- index.ts
+++ index.ts
@@ -3,13 +3,13 @@ import { config } from 'dotenv';
 
 config();
 
-async function connect(): Promise<Connection> {
+async function connect(database = 'DEMO_DB', schema = 'PUBLIC'): Promise<Connection> {
     const connection = createConnection({
-        account: process.env.SNOWFLAKE_ACCOUNT_NAME,
+        account: process.env.SNOWFLAKE_ACCOUNT,
         username: process.env.SNOWFLAKE_USERNAME,
         password: process.env.SNOWFLAKE_PASSWORD,
-        database: 'DEMO_DB',
-        schema: 'PUBLIC',
+        database,
+        schema,
     });
 
     return await new Promise((resolve, reject) =>
@@ -46,11 +46,11 @@ async function dropTable(connection: Connection): Promise<void> {
 async function runTest(connection: Connection, begin?: boolean): Promise<void> {
     await createTable(connection);
     try {
-        begin && await executeStatement(connection, 'BEGIN;');
+        begin && (await executeStatement(connection, 'BEGIN;'));
         await executeStatement(connection, 'INSERT INTO example VALUES (1);');
         await executeStatement(connection, 'INSERT INTO example VALUES (2);');
         await executeStatement(connection, 'COMMIT;');
-        begin && await executeStatement(connection, 'BEGIN;');
+        begin && (await executeStatement(connection, 'BEGIN;'));
         await executeStatement(connection, 'INSERT INTO example VALUES (3);');
         await executeStatement(connection, 'INSERT INTO example VALUES (4);');
         await executeStatement(connection, "INSERT INTO example VALUES ('ASD');");
@@ -74,7 +74,7 @@ async function main() {
     console.log('Running the test with the default connection.');
     await runTest(connection);
     console.log('');
-    
+
     console.log('Running the test with the default connection and BEGINs.');
     await runTest(connection, true);
     console.log('');
@@ -82,6 +82,23 @@ async function main() {
     console.log('Running the test without autocommit or BEGINs.');
     await executeStatement(connection, 'ALTER SESSION SET AUTOCOMMIT = FALSE;');
     await runTest(connection);
+    console.log('');
+
+    if (process.env.S3_LOCATION) {
+        const sample = await connect('SNOWFLAKE_SAMPLE_DATA', 'TPCH_SF100');
+
+        console.log('Running export.');
+        await executeStatement(sample, `
+            COPY INTO '${process.env.S3_LOCATION}' FROM "CUSTOMER"
+            credentials = (
+                aws_key_id='${process.env.S3_AWS_ACCESS_KEY_ID}' 
+                aws_secret_key='${process.env.S3_AWS_SECRET_ACCESS_KEY}' 
+                aws_token='${process.env.S3_AWS_SESSION_TOKEN}'
+            )
+            file_format = (type = csv);
+        `);
+        console.log('');
+    }
 }
 
 main().then(() => console.log('Done'));
